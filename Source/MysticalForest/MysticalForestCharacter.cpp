374c95cd78fad4122d76dea0f9d468a208814822
Bonchev Maxym <maxym.619@ukr.net>
2023-04-18 10:09:57 +0300

    Animations of Character

--- Source/MysticalForest/MysticalForestCharacter.cpp
+++ Source/MysticalForest/MysticalForestCharacter.cpp
@@ -30,10 +30,11 @@ AMysticalForestCharacter::AMysticalForestCharacter()
 
 	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
 	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
-	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetOwnerNoSee(true);
+	//Mesh1P->SetOnlyOwnerSee(true);
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
-	Mesh1P->bCastDynamicShadow = false;
-	Mesh1P->CastShadow = false;
+	Mesh1P->bCastDynamicShadow = true;
+	Mesh1P->CastShadow = true;
 	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
 	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
 
