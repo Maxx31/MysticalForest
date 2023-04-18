f8c69a729625b2c993e5c675c07db5bf1aab409c
Bonchev Maxym <maxym.619@ukr.net>
2023-04-18 15:29:11 +0300

    Added Interactable script

---
+++ Source/MysticalForest/Inventory/Interactable.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interactable.h"
+// Sets default values
+AInteractable::AInteractable()
+{
+	InteractableHelpText = FString("Press E to interact with item");
+}
+
+// Called when the game starts or when spawned
+void AInteractable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AInteractable::Interact_Implementation()
+{
+	GLog->Log("Interect base class: Interact() YOU SHOULD NOT BE SEEING THIS");
+}
+
